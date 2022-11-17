1247081dfcbc7a507c149e001442b9ff3f29e535
abrego <ale.abrego1214@gmail.com>
2022-11-17 12:57:03 -0600

    Funcionalidad de registro de datos

--- lib/widgets/dropdown.dart
+++ lib/widgets/dropdown.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Dropdown extends StatefulWidget {
-  Dropdown({Key? key}) : super(key: key);
+  Dropdown({
+    Key? key,
+    required this.lstitems,
+    required this.hintText,
+    required this.onChanged,
+    required this.selectedValue,
+    this.labelText,
+    
+  }) : super(key: key);
 
+  final Function(dynamic) onChanged;
+  final String? labelText;
+  final String hintText;
+  final dynamic selectedValue;
+  List<DropdownMenuItem<int>> lstitems;
   @override
   State<Dropdown> createState() => _DropdownState();
 }
@@ -11,33 +24,13 @@ class _DropdownState extends State<Dropdown> {
   @override
   Widget build(BuildContext context) {
     return DropdownButtonFormField(
-        decoration: const InputDecoration(
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(20.0)),
-              borderSide: BorderSide(color: Colors.grey, width: 0.0),
-            ),
-            border: OutlineInputBorder()),
-        items: [
-          const DropdownMenuItem(
-            child: Text("Masculino"),
-            value: 1,
-          ),
-          const DropdownMenuItem(
-            child: Text("Femenino"),
-            value: 2,
-          )
-        ],
-        hint: const Text("Sexo"),
-        onChanged: (value) {
-          setState(() {
-            //   measure = value;
-            // measureList.add(measure);
-          });
-        },
-        onSaved: (value) {
-          setState(() {
-            // measure = value;
-          });
-        });
+        decoration: InputDecoration(
+            labelText: widget.labelText,
+            hintText: widget.hintText,
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(5))),
+        items: widget.lstitems,
+        hint: Text(widget.hintText),
+        onChanged: widget.onChanged,
+        );
   }
 }
