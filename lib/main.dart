2e3b730d4ab76210ebc6c2ba1e18be398e99ce82
abrego <ale.abrego1214@gmail.com>
2022-11-17 21:38:20 -0600

    eliminando clase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:parcial4app/paginas/HomePage.dart';
 import 'package:parcial4app/paginas/formReserva.dart';
 import 'package:parcial4app/principal.dart';
 
